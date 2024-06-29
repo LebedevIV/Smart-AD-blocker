44caf58a4632da7e399d762f68380a556b8c54a6
Igor Lebedev <igor_samodurov@mail.ru>
2024-06-30 02:41:02 +0500

    Update Smart AD  blocker.js

--- Extensions/UserScript/Smart AD  blocker.js
+++ Extensions/UserScript/Smart AD  blocker.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Smart AD blocker for: yandex, mail.ru
+// @name         Smart AD blocker for: Yandex, Mail.ru
 // @namespace    http://tampermonkey.net/
 // @version      2024-06-30_02-29
 // @description  try to take over the world!
