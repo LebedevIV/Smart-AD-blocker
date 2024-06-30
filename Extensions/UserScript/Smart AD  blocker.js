8bf37edf13df84a91f9e6fd0aa17ee985c3ec918
Igor Lebedev <igor_samodurov@mail.ru>
2024-07-01 00:11:10 +0500

    Update Smart AD  blocker.js

--- Extensions/UserScript/Smart AD  blocker.js
+++ Extensions/UserScript/Smart AD  blocker.js
@@ -3,8 +3,8 @@
 // @name:ru         Умный блокировщик рекламы для: Yandex, Mail.ru
 // @namespace    http://tampermonkey.net/
 // @version      2024-06-30_04-03
-// @description  Smart AD blocker for dynamic blocking protection, for: Yandex, Mail.ru
-// @description:ru  Умный блокировщик рекламы для динамической защиты от блокировки, для: Yandex, Mail.ru
+// @description  Smart AD blocker with dynamic blocking protection, for: Yandex, Mail.ru
+// @description:ru  Умный блокировщик рекламы при динамической защите от блокировки, для: Yandex, Mail.ru
 // @author       Igor Lebedev
 // @license        GPL-3.0-or-later
 // @match        http://*.mail.ru/*
